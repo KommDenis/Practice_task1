aa2a2f8aff15323f7aa1901e677748640001d20a
KommDenis <kommdenis@gmail.com>
2024-06-28 13:43:48 +0300

    form made

--- practice_task1/CustomNoSqlDb.cs
+++ practice_task1/CustomNoSqlDb.cs
@@ -1,85 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Xml;
 using Newtonsoft.Json;
 
-public class CustomNoSqlDb
+namespace practice_task1
 {
-    private readonly string _databasePath;
-
-    public CustomNoSqlDb(string databasePath)
+    public class CustomNoSqlDb
     {
-        _databasePath = databasePath;
-        if (!Directory.Exists(_databasePath))
-        {
-            Directory.CreateDirectory(_databasePath);
-        }
-    }
+        private readonly string _databasePath;
 
-    private string GetCollectionPath(string collectionName) => Path.Combine(_databasePath, $"{collectionName}.json");
-
-    private Dictionary<Guid, string> LoadCollection(string collectionName)
-    {
-        var collectionPath = GetCollectionPath(collectionName);
-        if (!File.Exists(collectionPath))
+        public CustomNoSqlDb(string databasePath)
         {
-            return new Dictionary<Guid, string>();
+            _databasePath = databasePath;
+            if (!Directory.Exists(_databasePath))
+            {
+                Directory.CreateDirectory(_databasePath);
+            }
         }
 
-        var json = File.ReadAllText(collectionPath);
-        return JsonConvert.DeserializeObject<Dictionary<Guid, string>>(json);
-    }
+        private string GetCollectionPath(string collectionName) => Path.Combine(_databasePath, $"{collectionName}.json");
 
-    private void SaveCollection(string collectionName, Dictionary<Guid, string> collection)
-    {
-        var collectionPath = GetCollectionPath(collectionName);
-        var json = JsonConvert.SerializeObject(collection, Newtonsoft.Json.Formatting.Indented);
-        File.WriteAllText(collectionPath, json);
-    }
-
-    public void Insert<T>(T document, string collectionName)
-    {
-        var collection = LoadCollection(collectionName);
-        var id = Guid.NewGuid();
-        var json = JsonConvert.SerializeObject(document);
-        collection[id] = json;
-        SaveCollection(collectionName, collection);
-    }
-
-    public T GetById<T>(Guid id, string collectionName)
-    {
-        var collection = LoadCollection(collectionName);
-        if (collection.TryGetValue(id, out var json))
+        private Dictionary<Guid, string> LoadCollection(string collectionName)
         {
-            return JsonConvert.DeserializeObject<T>(json);
+            var collectionPath = GetCollectionPath(collectionName);
+            if (!File.Exists(collectionPath))
+            {
+                return new Dictionary<Guid, string>();
+            }
+
+            var json = File.ReadAllText(collectionPath);
+            return JsonConvert.DeserializeObject<Dictionary<Guid, string>>(json);
         }
-        return default(T);
-    }
 
-    public void Update<T>(Guid id, T document, string collectionName)
-    {
-        var collection = LoadCollection(collectionName);
-        var json = JsonConvert.SerializeObject(document);
-        collection[id] = json;
-        SaveCollection(collectionName, collection);
-    }
-
-    public void Delete(Guid id, string collectionName)
-    {
-        var collection = LoadCollection(collectionName);
-        if (collection.Remove(id))
+        private void SaveCollection(string collectionName, Dictionary<Guid, string> collection)
         {
+            var collectionPath = GetCollectionPath(collectionName);
+            var json = JsonConvert.SerializeObject(collection, Formatting.Indented);
+            File.WriteAllText(collectionPath, json);
+        }
+
+        public void Insert<T>(T document, string collectionName)
+        {
+            var collection = LoadCollection(collectionName);
+            var idProperty = typeof(T).GetProperty("Id");
+            if (idProperty == null)
+            {
+                throw new Exception("Document must have an Id property");
+            }
+            var id = (Guid)idProperty.GetValue(document);
+            var json = JsonConvert.SerializeObject(document);
+            collection[id] = json;
             SaveCollection(collectionName, collection);
         }
-    }
 
-    public IEnumerable<T> GetAll<T>(string collectionName)
-    {
-        var collection = LoadCollection(collectionName);
-        foreach (var json in collection.Values)
+        public T GetById<T>(Guid id, string collectionName)
         {
-            yield return JsonConvert.DeserializeObject<T>(json);
+            var collection = LoadCollection(collectionName);
+            if (collection.TryGetValue(id, out var json))
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            return default(T);
+        }
+
+        public void Update<T>(Guid id, T document, string collectionName)
+        {
+            var collection = LoadCollection(collectionName);
+            var json = JsonConvert.SerializeObject(document);
+            collection[id] = json;
+            SaveCollection(collectionName, collection);
+        }
+
+        public void Delete(Guid id, string collectionName)
+        {
+            var collection = LoadCollection(collectionName);
+            if (collection.Remove(id))
+            {
+                SaveCollection(collectionName, collection);
+            }
+        }
+
+        public IEnumerable<dynamic> GetAll(string collectionName)
+        {
+            var collection = LoadCollection(collectionName);
+            foreach (var entry in collection)
+            {
+                var json = entry.Value;
+                var document = JsonConvert.DeserializeObject<dynamic>(json);
+                document.Id = entry.Key;
+                yield return document;
+            }
         }
     }
 }
--- practice_task1/Form1.cs
+++ practice_task1/Form1.cs
@@ -1,20 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace practice_task1
 {
     public partial class Form1 : Form
     {
+        private readonly CustomNoSqlDb _db;
+
         public Form1()
         {
             InitializeComponent();
+            _db = new CustomNoSqlDb("MyDatabase");
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var people = _db.GetAll("people").ToList();
+            var peopleWithGuid = people.Select(p => new { Id = Guid.Parse(p.Id.ToString()), Name = p.Name, Age = p.Age }).ToList();
+            dataGridView1.DataSource = peopleWithGuid;
+
+            if (!dataGridView1.Columns.Contains("Id"))
+            {
+                dataGridView1.Columns.Add("Id", "Id");
+            }
+            dataGridView1.Columns["Id"].Visible = false; // Приховати стовпець Id
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var document = new { Id = Guid.NewGuid(), Name = textBoxName.Text, Age = int.Parse(textBoxAge.Text) };
+            _db.Insert(document, "people");
+            LoadData();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                if (selectedRow.Cells["Id"].Value is Guid id)
+                {
+                    var document = new { Id = id, Name = textBoxName.Text, Age = int.Parse(textBoxAge.Text) };
+                    _db.Update(id, document, "people");
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                if (selectedRow.Cells["Id"].Value is Guid id)
+                {
+                    _db.Delete(id, "people");
+                    LoadData();
+                }
+            }
         }
     }
 }
